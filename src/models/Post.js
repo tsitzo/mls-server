4fb329d6c5ea776dbc4503d520e7a4bfb3d3acf5
Federico Giorgino <federicog.dev@gmail.com>
2022-07-19 15:40:13 +0200

    Post Model

---
+++ src/models/Post.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+const { Schema } = mongoose;
+
+const postSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  text: {
+    type: String,
+    required: true,
+  },
+  likes: [{ type: Schema.Types.ObjectId, ref: "User" }],
+  agrees: [{ type: Schema.Types.ObjectId, ref: "User" }],
+  deserves: [{ type: Schema.Types.ObjectId, ref: "User" }],
+  comments: [
+    {
+      text: { type: String, required: true },
+      user: { type: Schema.Types.ObjectId, ref: "User" },
+      username: { type: String, required: true },
+      createdAt: { type: Date, default: Date.now },
+    },
+  ],
+  pending: { type: Boolean, default: true },
+  approved: { type: Boolean, default: false },
+  createdAt: { type: Date, default: Date.now },
+});
+
+mongoose.model("Post", postSchema);
