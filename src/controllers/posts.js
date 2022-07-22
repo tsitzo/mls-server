6b9a395263ad8af0cffdbc1642e829bebc72bb80
Federico Giorgino <federicog.dev@gmail.com>
2022-07-22 16:15:42 +0200

    posts controllers getPosts

--- src/controllers/posts.js
+++ src/controllers/posts.js
@@ -4,6 +4,26 @@ const mongoose = require("mongoose");
 const User = mongoose.model("User");
 const Post = mongoose.model("Post");
 
+exports.getPosts = async (req, res) => {
+  const perPage = 2;
+  const page = req.params.page ? req.params.page : 1;
+
+  try {
+    const allPosts = await Post.find({
+      approved: true,
+      pending: false,
+    })
+      .skip((page - 1) * perPage)
+      .populate("user", "username image")
+      .sort({ createdAt: -1 })
+      .limit(perPage);
+
+    res.status(200).send(allPosts);
+  } catch (error) {
+    return res.status(500).send({ error: "Server Error" });
+  }
+};
+
 exports.createPost = async (req, res) => {
   const { id } = req.user;
   const { text, title } = req.body;
